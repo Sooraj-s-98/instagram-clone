ba87b7b6a697a9c529c94ecfe86b25584d128f5c
sooraj s <sooraj1998ayiroor@gmail.com>
2020-08-23 19:27:48 +0530

    JWT token

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router=express.Router()
 const mongoose=require('mongoose')
 const User= mongoose.model("User")
 const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
+const {JWT_SECRET}=require('../keys')
 
 router.post('/signup',(req,res)=>{
     const {name,email,password}=req.body
@@ -49,7 +51,9 @@ router.post('/signin',(req,res)=>{
       bcrypt.compare(password,savedUser.password)
       .then(doMatch=>{
           if(doMatch){
-              res.json({message:"succsessfully signed in"})
+              //res.json({message:"succsessfully signed in"})
+              const token=jwt.sign({_id:savedUser._id},JWT_SECRET)
+              res.json({token})
           }
           else{
             res.status(422).json({error:"Invaild Email or password"})
