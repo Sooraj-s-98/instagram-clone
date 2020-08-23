d8c3a2612ba488480c6e594008b61f4096679367
sooraj s <sooraj1998ayiroor@gmail.com>
2020-08-23 19:10:55 +0530

    signin and signup

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app=express()
 const PORT=5000
 const{MONGOURI}=require('./keys')
 
+require('./models/user')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+
 mongoose.connect(MONGOURI,{
     useNewUrlParser: true,
     useUnifiedTopology: true
---
+++ routes/auth.js
@@ -0,0 +1,64 @@
+const express=require('express')
+const router=express.Router()
+const mongoose=require('mongoose')
+const User= mongoose.model("User")
+const bcrypt=require('bcryptjs')
+
+router.post('/signup',(req,res)=>{
+    const {name,email,password}=req.body
+    if(!email|| !password || !name){
+       return( res.status(422).json({error:"please add all the fields"}) )
+    }
+  User.findOne({email:email})
+  .then((savedUser)=>{
+      if(savedUser){
+          return res.status(422).json({error:"user already exists with that email"})
+      }
+      bcrypt.hash(password,12)
+      .then(hashedpassword=>{
+        const user=new User({
+            email,
+            password :hashedpassword,
+            name
+        })
+        user.save()
+        .then(user=>{
+            res.json({message:"saved successfully"})
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+      })
+
+  })
+  .catch(err=>{
+      console.log(err)
+  })
+})
+
+router.post('/signin',(req,res)=>{
+    const {email,password}=req.body
+    if(!email|| !password ){
+       res.status(422).json({error:"please add email and password"}) 
+    }
+  User.findOne({email:email})
+  .then((savedUser)=>{
+      if(!savedUser){
+         res.status(422).json({error:"Invaild Email or password"})
+      }
+      bcrypt.compare(password,savedUser.password)
+      .then(doMatch=>{
+          if(doMatch){
+              res.json({message:"succsessfully signed in"})
+          }
+          else{
+            res.status(422).json({error:"Invaild Email or password"})
+          }
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    })
+})
+
+module.exports=router
