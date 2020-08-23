488dbf8fa7ee5cb4cf2929ba3dd93445d7748c12
sooraj s <sooraj1998ayiroor@gmail.com>
2020-08-23 15:37:47 +0530

    middleware

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express=require("express")
 const app=express()
 const PORT=5000
 
+const customMiddleware=(req,res,next)=>{
+    console.log("Middleware executed")
+    next()
+}
+
 app.get('/',(req,res)=>{
+    console.log("home")
     res.send("hello")
+})
+
+app.get('/about',customMiddleware,(req,res)=>{
+    console.log("about")
+    res.send("about page")
 })
 
 app.listen(PORT,()=>{
