3fd2626e1a7901abccfd1c049f39cdf6ade11e20
sooraj s <sooraj1998ayiroor@gmail.com>
2020-08-24 18:56:00 +0530

    home , login,profile,navbar UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,47 @@ import React from 'react';
 
 const Home = ()=>{
     return(
-        <h1>HOME</h1>
+        <div className="home">
+                    <div className="card home-card">
+                         <h5 style={{padding:"5px"}}>my post</h5>
+                         <div className="card-image">
+                             <img src="https://images.unsplash.com/photo-1511310651328-d5d59cf47b88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
+                         </div>
+                         <div className="card-content">
+                         <i className="material-icons" style={{color:"red"}}>favorite</i>                      
+                             <h6>6 likes</h6>
+                             <h6>title</h6>
+                             <p>this is amazing post</p>
+                             <input type="text" placeholder="add a comment" />  
+                         </div>
+                     </div>   
+                     <div className="card home-card">
+                         <h5 style={{padding:"5px"}}>my post</h5>
+                         <div className="card-image">
+                             <img src="https://images.unsplash.com/photo-1511310651328-d5d59cf47b88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
+                         </div>
+                         <div className="card-content">
+                         <i className="material-icons" style={{color:"red"}}>favorite</i>                      
+                             <h6>6 likes</h6>
+                             <h6>title</h6>
+                             <p>this is amazing post</p>
+                             <input type="text" placeholder="add a comment" />  
+                         </div>
+                     </div>   
+                     <div className="card home-card">
+                         <h5 style={{padding:"5px"}}>my post</h5>
+                         <div className="card-image">
+                             <img src="https://images.unsplash.com/photo-1511310651328-d5d59cf47b88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
+                         </div>
+                         <div className="card-content">
+                         <i className="material-icons" style={{color:"red"}}>favorite</i>                      
+                             <h6>6 likes</h6>
+                             <h6>title</h6>
+                             <p>this is amazing post</p>
+                             <input type="text" placeholder="add a comment" />  
+                         </div>
+                     </div>   
+       </div>
     )
 
 }
--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = ()=>{
     return(
-        <h1>login</h1>
+        <div className="mycard">
+           <div className="card auth-card input-field">
+               <h2>Instagram</h2>
+               <input
+                 type="text"
+                 placeholder="email"
+               />
+               <input
+                 type="password"
+                 placeholder="password"
+               />
+               <button className="btn waves-effect waves-light #64b5f6 blue darken-1" >
+                   Login
+               </button>
+               <h5><Link to="/signup">Dont have an account ?</Link></h5>
+              <h6> <Link to="">Forgot password ?</Link></h6>
+           </div>
+        </div>
     )
 
 }
