85ce2bc4121997d29828e4787ccdbe39ba8810de
sooraj s <sooraj1998ayiroor@gmail.com>
2020-08-24 10:37:13 +0530

    post peding

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ const app=express()
 const PORT=5000
 const{MONGOURI}=require('./keys')
 
-require('./models/user')
-
-app.use(express.json())
-app.use(require('./routes/auth'))
-
 mongoose.connect(MONGOURI,{
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -21,6 +16,14 @@ mongoose.connection.on('connected',()=>{
 mongoose.connection.on('error',()=>{
     console.log("error connection ",err)
 })
+
+require('./models/user')
+require('./models/post')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+
 //PQF1T8YtyokLtVkx
 
 app.listen(PORT,()=>{
